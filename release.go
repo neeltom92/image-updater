848b909833290fe30e709729e51e36d5559b7481
Neel Thomas <neelthomas@Neels-MacBook-Pro.local>
2023-03-29 15:47:26 -0700

    Fix: updates

--- release.go
+++ release.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/object"
-	"github.com/go-git/go-git/v5/plumbing/semver"
+	"github.com/Masterminds/semver"
 )
 
 func main() {
